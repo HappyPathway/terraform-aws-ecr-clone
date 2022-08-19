e404b6f7d2fb4c94b8c08dba04085ae4ff9a17f2
badra001 <donald.e.badrak.ii@census.gov>
2022-08-19 13:04:38 -0400

    fix

--- main.tf
+++ main.tf
@@ -54,7 +54,7 @@ locals {
   repo_parent_name = format("%v", var.application_name)
   region           = var.region == null ? data.aws_region.current.name : var.region
 
-  account_ecr_registry = format("%v.dkr.ecr.%v.amazonaws.com", local.account_id, local.ecr_region)
+  account_ecr_registry = format("%v.dkr.ecr.%v.amazonaws.com", local.account_id, local.region)
   account_ecr          = format("%v/%v", local.account_ecr_registry, local.repo_parent_name)
 
   images = { for i in var.image_config : format("%v#%v", i.name, i.tag) =>
@@ -79,7 +79,7 @@ resource "null_resource" "copy_images" {
     command = "${path.module}/bin/copy_image.sh"
     environment = {
       AWS_PROFILE          = var.profile
-      AWS_REGION           = local.ecr_region
+      AWS_REGION           = local.region
       SOURCE_IMAGE         = each.value.source_full_path
       DESTINATION_IMAGE    = each.value.dest_full_path
       SOURCE_USERNAME      = var.source_username == null ? "" : var.source_username
