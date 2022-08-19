3dbe69a254fddb5031a7b845e4fd4b798fcb7d15
badra001 <donald.e.badrak.ii@census.gov>
2022-08-19 12:51:21 -0400

    update

--- main.tf
+++ main.tf
@@ -1,3 +1,46 @@
+/*
+* # aws-ecr-copy-images
+*
+* # Usage
+#
+*/
+
+locals {
+  application_list = var.application_list
+  ecr_repo_list    = { for app in local.application_list : app => format("%v/%v", var.application_name, app) }
+}
+
+#---
+# craete reposs if list present
+#---
+resource "aws_ecr_repository" "apps_repos" {
+  for_each = local.ecr_repo_list
+  name     = each.value
+
+  image_tag_mutability = "IMMUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  encryption_configuration {
+    encryption_type = "KMS"
+  }
+
+  tags = merge(
+    local.common_tags,
+    local.base_tags,
+    var.application_tags,
+    tomap({
+      "Name"        = format("ecr_%v/%v", var.application_name, each.key)
+      "Environment" = "application"
+    }),
+  )
+}
+
+#---
+# copy images
+#---
+
 data "aws_ecr_authorization_token" "token" {}
 
 # ECR format
@@ -45,10 +88,4 @@ resource "null_resource" "copy_images" {
       DESTINATION_PASSWORD = var.destination_password == null ? data.aws_ecr_authorization_token.token.password : var.destination_password
     }
   }
-}
-
-
-output "images" {
-  description = "Final full merge of images with extra details"
-  value       = local.images
 }
